5c6622b64b9694e191c5ea893e26f159cb190094
David Patlut <dpatlut@gmail.com>
2025-06-12 14:05:52 -0400

    Update app.py

--- geoapp/app.py
+++ geoapp/app.py
@@ -9,7 +9,7 @@ import config
 host = config.REDIS_CFG["host"]
 port = config.REDIS_CFG["port"]
 pwd = config.REDIS_CFG["password"]
-redis = Redis(host=host, port=port, password=pwd, charset="utf-8", decode_responses=True)
+redis = Redis(host=host, port=port, password=pwd, decode_responses=True)
 
 # Flask Application
 app = Flask(__name__)
